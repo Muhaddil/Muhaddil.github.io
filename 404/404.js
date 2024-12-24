e19c181311fdca307acf4dac1e8e973dfc8e9ef2
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2024-12-24 17:49:02 +0100

    Update 6.68 - Enhance 404 page with additional logic for smoother transitions and decreased fade-in time for normal loading.

--- 404/404.js
+++ 404/404.js
@@ -8,16 +8,12 @@ const MIN_PERCENTAGE = 30;
 const MAX_PERCENTAGE = 70;
 const PAGE_LOAD_DELAY = 3500;
 const ANIMATION_DURATION = 1600;
-const REDIRECT_URL = "/";
+const REDIRECT_URL = "/?fade=true";
 
 // Function to change the gradient
 function changeGradient(x) {
-  const bgWebKit = `linear-gradient(${GRADIENT_ANGLE}deg, ${COLOR_START} ${x}%, ${COLOR_END} ${
-    x + 10
-  }%)`;
-  const bgMoz = `linear-gradient(${GRADIENT_ANGLE}deg, ${COLOR_START} ${x}%, ${COLOR_END} ${
-    x + 10
-  }%)`;
+  const bgWebKit = `linear-gradient(${GRADIENT_ANGLE}deg, ${COLOR_START} ${x}%, ${COLOR_END} ${x + 10}%)`;
+  const bgMoz = `linear-gradient(${GRADIENT_ANGLE}deg, ${COLOR_START} ${x}%, ${COLOR_END} ${x + 10}%)`;
   document.body.style.backgroundImage = bgWebKit;
   document.body.style.backgroundImage = bgMoz;
 }
@@ -33,12 +29,12 @@ document.body.addEventListener("mousemove", function (e) {
     x = 50 - x; // Adjusts the center of the gradient
     x = Math.min(Math.max(x, MIN_PERCENTAGE), MAX_PERCENTAGE); // Limits x to a range of 30 to 70
     changeGradient(x);
-  });  
+});
 
-// Add a fade duration
+// Fade effect duration
 const FADE_DURATION = 1000;
 
-// Fade start function
+// Function to start fade-out
 function startFadeOut() {
   document.body.style.transition = `opacity ${FADE_DURATION}ms`;
   document.body.style.opacity = 0;
@@ -46,18 +42,24 @@ function startFadeOut() {
 
 // Modify the window loading function
 window.onload = function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  
+  // Adjust the fade duration based on the query parameter
+  const fadeDuration = urlParams.get('fade') === 'true' ? 2000 : 1000; // Long fade for 'fade=true', default for 'fade=false'
+
   setTimeout(function () {
     const img = document.querySelector("img");
     img.classList.add("fly-away");
 
     // Start fading before redirection
     setTimeout(function () {
+      document.body.style.transition = `opacity ${fadeDuration}ms`;
       startFadeOut();
 
       // Wait for the fade to complete before redirecting
       setTimeout(function () {
-        window.location.href = REDIRECT_URL;
-      }, FADE_DURATION);
+        window.location.href = REDIRECT_URL; // Redirigir con fade=true
+      }, fadeDuration);
     }, ANIMATION_DURATION);
   }, PAGE_LOAD_DELAY);
 };
